7c40adf51dfe86badb49f807f457a12757ec287a
Laxman Giri <laxmanvatij@gmail.com>
2024-12-23 21:58:24 +0100

    addedmiddleware

--- Client/src/app/store.js
+++ Client/src/app/store.js
@@ -1,10 +1,13 @@
-import authSlice from "@/features/authSlice"
-import { configureStore } from "@reduxjs/toolkit"
+import authSlice from "@/features/authSlice";
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./rootReducer";
+import { authApi } from "@/features/api/authApi";
 
-
-const appStore=configureStore({
-    reducer:{
-        authSlice:authSlice
-    }
-})
-export default appStore
+const appStore = configureStore({
+  reducer: {
+    authSlice: authSlice,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(authApi.middleware),
+});
+export default appStore;
